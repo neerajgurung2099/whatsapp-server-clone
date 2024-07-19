80d6837d876cd3a1c9e22e7cf91b2bfc68c34289
neerajgurung2099 <neerajgurung2099@gmail.com>
2024-07-20 01:39:17 +0545

    Add allowed domain in cors

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -10,7 +10,18 @@ export default ({ app }: { app: express.Application }) => {
   app.head("/status", (req, res) => {
     res.status(200).end();
   });
-  app.use(cors());
+
+  const allowedDomains = ["https://whatsapp.neerajgurung.com.np"];
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (allowedDomains.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  };
+  app.use(cors(corsOptions));
   app.use(express.json());
   app.use(config.api.prefix, routes());
 
