92fc520735d5fd29add501d3f1f3aa504478a316
neerajgurung2099 <neerajgurung2099@gmail.com>
2024-07-20 01:28:41 +0545

    Search for .env file only in development

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-const envFound = dotenv.config();
-if (envFound.error) {
-  // crash the whole process
+if ((process.env.NODE_ENV = "development")) {
+  const envFound = dotenv.config();
+  if (envFound.error) {
+    // crash the whole process
 
-  throw new Error("Could'nt find .env file");
+    throw new Error("Could'nt find .env file");
+  }
 }
 export default {
   port: parseInt(process.env.PORT, 10),
